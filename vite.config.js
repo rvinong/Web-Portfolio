6654ee40fa981c1023bfebb0dd808504f4ba1509
Rvin Ong Labrada <ongrvin@gmail.com>
2026-07-11 09:51:03 +0800

    feat(portfolio): add certification archive

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        resume: "resume.html",
+        certifications: "certifications.html",
+      },
+    },
+  },
 });
